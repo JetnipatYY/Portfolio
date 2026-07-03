4f2aa4dbb362b6422c5fc0ffe5d12e4b9261c7f8
Jetnipat yongyuth <jetnipat.y@ku.th>
2026-07-03 16:40:12 +0700

    commit 3/7/2568

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,8 @@
+import ProfileImg from "./assets/images/Profile.jpg";
+import Notebook from "./assets/images/Notebook.png";
+import GithubIcon from "./assets/images/github.png";
+import LinkedinIcon from "./assets/images/linkedin.png";
+
 import './App.css'
 
 function App() {
@@ -28,16 +33,16 @@ function App() {
         <ul className="nav-list">
           <li className="nav-item">
             <a
-              href="#Intro"
+              href="#Home"
               onClick={(e) => {
                 e.preventDefault();
-                document.getElementById("Intro").scrollIntoView({
+                document.getElementById("Home").scrollIntoView({
                   behavior: "smooth",
                   active: "var(--peach)",
                 });
               }}
             >
-              Introduction
+              Home
             </a>
           </li>
           <li className="nav-item">
@@ -80,7 +85,7 @@ function App() {
               }}
             >
               Skills
-            </a>  
+            </a>
           </li>
           <li className="nav-item">
             <a
@@ -113,97 +118,39 @@ function App() {
         </ul>
       </section>
 
-      <section className="intro" id="Intro">
-        <div className="intro-content">
-
-          <h1 className="intro-title">
+      <section className="Home" id="Home">
+        <div className="Home-content">
+          <h1 className="Home-title">
             👋🏻Hello, I'm <span className="highlight">Jetnipat yongyuth</span>
           </h1>
 
-          <p className="intro-subtitle"> Web developer. </p>
+          <p className="Home-subtitle">
+            {" "}
+            Web developer.
+            <img src={Notebook} alt="Notebook" className="notebook-img" />
+          </p>
+
+          <h1>
+            I am a Bachelor of Science graduate in Information Technology with a
+            strong interest in Developer roles. I specialize in Front-end
+            development and UI/UX design, and I am eager to bring my knowledge
+            and experience to contribute meaningfully to your organization .
+          </h1>
 
           <button className="download-btn">Download Resume</button>
 
           <div className="contact-btn">
             {/* ----- GitHub button ----- */}
             <button className="github-btn">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                className="lucide lucide-github-icon lucide-github"
-              >
-                <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" />
-                <path d="M9 18c-4.51 2-5-2-7-2" />
-              </svg>
-
-              {/* <img src="/assets/images/github.png" alt="github" /> */}
+              <img src={GithubIcon} alt="github" className="contact-icon" />
             </button>
 
             {/* ----- LinkedIn button ----- */}
             <button className="linkedin-btn">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                className="lucide lucide-linkedin-icon lucide-linkedin"
-              >
-                <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" />
-                <rect width="4" height="12" x="2" y="9" />
-                <circle cx="4" cy="4" r="2" />
-              </svg>
+              <img src={LinkedinIcon} alt="linkedin" className="contact-icon" />
             </button>
 
-            {/* ----- Facebook button ----- */}
-            <button className="facebook-btn">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                className="lucide lucide-facebook-icon lucide-facebook"
-              >
-                <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" />
-              </svg>
-            </button>
-
-            {/* ----- Instagram button ----- */}
-            <button className="instagram-btn">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                className="lucide lucide-instagram-icon lucide-instagram"
-              >
-                <rect width="20" height="20" x="2" y="2" rx="5" ry="5" />
-                <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" />
-                <line x1="17.5" x2="17.51" y1="6.5" y2="6.5" />
-              </svg>
-            </button>
-
+            {/* ----- Phone button ----- */}
             <button className="phone-btn">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -224,12 +171,9 @@ function App() {
           </div>
         </div>
 
+        {/* ----- Profile image ----- */}
         <div className="profile-image">
-          <img
-            src="./assets/images/ProfilePort.jpg"
-            alt="Profile"
-            className="profile-img"
-          />
+          <img src={ProfileImg} alt="Profile" className="profile-img" />
         </div>
       </section>
 
@@ -237,8 +181,9 @@ function App() {
       <section className="about" id="about">
         <h2 className="section-title">About Me</h2>
         &nbsp; {/* &nbsp is used to space the words in line*/}
+        <h2 className="section-title" >Know More About Me </h2>
         <p className="section-description">
-          I am a web developer with a passion for creating beautiful and
+           I'm a Passionate Web Developer for creating beautiful and
           functional websites. I have experience in HTML, CSS, JavaScript, and
           React.
         </p>
@@ -246,7 +191,7 @@ function App() {
 
       {/*----- Education section -----*/}
       <section className="education" id="education">
-        <h2 className="experience-title">Education</h2>
+        <h2 className="section-title">Education</h2>
         &nbsp;
         <div className="Kasetsart-profile">
           <img
@@ -274,52 +219,48 @@ function App() {
 
       {/* ----- Experience section ----- */}
       <section className="experience" id="experience">
-
-        <h2 className="experience-title">Experience</h2>
+        <h2 className="section-title">Experience</h2>
         <p className="experience-description">
-            this is my experience in web development and related fields. I have
-            worked on various projects and have gained valuable experience in
-            the industry.
+          this is my experience in web development and related fields. I have
+          worked on various projects and have gained valuable experience in the
+          industry.
         </p>
 
         {/*-------  Carrousel for experience images  -------*/}
         <div className="carousel-ctn">
-          <div className="left-btn"></div>
+          <div className="left-btn"> 
+            <img>
+            </img>
+           </div>
 
-            <div className="carousel-item">
-
-              
-              <img
-                src="./assets/images/experience1.png"
-                alt="Experience 1"
-                className="experience-img"
-              />
-                <h3 className="experience-subtitle">Web Development Experience</h3>
-              <p></p>
-
-            </div>
-
-            <div className="carousel-item">
-              <img
-                src="./assets/images/experience2.png"
-                alt="Experience 2"
-                className="experience-img"
-              />
-            </div>
-
-            <div className="carousel-item">
-              <img
-                src="./assets/images/experience3.png"
-                alt="Experience 3"
-                className="experience-img"
-              />
-            </div>
-
+          <div className="carousel-item">
+            <img
+              src="./assets/images/experience1.png"
+              alt="Experience 1"
+              className="experience-img"
+            />
+            <h3 className="experience-subtitle">Web Development Experience</h3>
+            <p></p>
           </div>
-          <div className="right-btn"></div>
 
+          <div className="carousel-item">
+            <img
+              src="./assets/images/experience2.png"
+              alt="Experience 2"
+              className="experience-img"
+            />
+          </div>
+
+          <div className="carousel-item">
+            <img
+              src="./assets/images/experience3.png"
+              alt="Experience 3"
+              className="experience-img"
+            />
+          </div>
+        </div>
+        <div className="right-btn"></div>
       </section>
-
 
       {/* ----- Contact section ----- */}
       <section className="contact" id="contact">
@@ -330,7 +271,6 @@ function App() {
           opportunities!
         </p>
       </section>
-
     </>
   );
 }
